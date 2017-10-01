4da692b895080372d753434bab82dbd7da06ff49
concepcc27 <concepcc27@gmail.com>
2017-10-01 17:45:23 -0400

    UI additions

--- src/components/main/MainPage.js
+++ src/components/main/MainPage.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import MainNavBar from './MainNavBar';
 import MainToolBar from './MainToolBar';
+import '../../assets/scss/_MainPage.scss';
 
 class MainPage extends Component {
   render() {
     return (
-      <div className=''>
+      <div className='main-page'>
         <MainNavBar />
         <MainToolBar />
       </div>
