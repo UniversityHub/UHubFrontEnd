61665dc37fcdc7ce988e6f286015b9cb0ab1e816
tkotekar <tejaswi.kotekar@gmail.com>
2017-11-02 16:48:17 -0400

    Todo list Front end complete

--- src/components/ToDoListService.js
+++ src/components/ToDoListService.js
@@ -14,6 +14,8 @@ class ToDoItemService {
   }
 
   addEntry = (user, entries) => {
+    console.log(user);
+    console.log(entries);
     axios.post('http://localhost:4200/todoList/add-entry', {
       userID: user,
       items: entries,
@@ -23,6 +25,32 @@ class ToDoItemService {
     })
     .catch(err => console.log(err))
   }
+
+  deleteEntry = (user, entries) => {
+    console.log(user);
+    console.log(entries);
+    axios.post('http://localhost:4200/todoList/delete-entry', {
+      userID: user,
+      items: entries,
+    })
+    .then(res => {
+      console.log('entry successfully deleted');
+    })
+    .catch(err => console.log(err))
+  }
+
+  /*saveTask = (entry) => {
+    axios.post('http://localhost:4200/todoList/saveTask', {
+      //userID: user,
+      entries: entry
+    })
+    .then(res => {
+      console.log(res);
+      // this.setState({ todoList: res.data })
+    })
+    .catch(err => console.log(err))
+  }*/
+
 }
 
 export default ToDoItemService;
