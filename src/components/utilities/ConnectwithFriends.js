1687306d4a04f499be8346cbe46dfa3417f2a201
concepcc27 <concepcc27@gmail.com>
2017-12-01 01:11:22 -0500

    initial friend info

--- src/components/utilities/ConnectwithFriends.js
+++ src/components/utilities/ConnectwithFriends.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ConnectwithFriendsService from '../ConnectwithFriendsService';
+import UserInfoService from '../UserInfoService';
+import PiazzaService from '../PiazzaService';
+
+import Modal from 'react-responsive-modal';
 
 class ConnectwithFriends extends Component {
   state = {
     friends: [],
     allUsers: [],
+    friendModal: [false, '']
   }
 
   constructor(props) {
     super(props);
     this.addConnectwithFriendsService = new ConnectwithFriendsService();
+    this.UserInfoService = new UserInfoService();
+    this.PiazzaService = new PiazzaService();
   }
 
   componentWillMount() {
@@ -28,12 +35,18 @@ class ConnectwithFriends extends Component {
         let final = arr.map((elem, index) => {
           return elem.userID;
         })
-        console.log(final);
+        //console.log(final);
         this.setState({allUsers: final});
       })
       .catch(err => console.log(err));
   }
 
+  handleCloseModal = () => {
+    const arr = [false, ''];
+    this.setState({friendModal: arr});
+  }
+
+  //CONNECT TO USER
   handleConnect = (index) => {
     console.log("Trying to connect");
     const { allUsers, friends } = this.state;
@@ -43,6 +56,14 @@ class ConnectwithFriends extends Component {
 
     this.addConnectwithFriendsService.addFriend(this.props.user, friendArr, friend);
     this.setState({friends: friendArr});
+  }
+
+  //CHECK FRIEND INFO
+  handleInfo = (index) => {
+    const modalArr = this.state.friendModal.slice();
+    modalArr[0] = true;
+    modalArr[1] = this.state.friends[index];
+    this.setState({friendModal: modalArr})
   }
 
   displayFriends = () => {
@@ -57,7 +78,7 @@ class ConnectwithFriends extends Component {
     }
     return this.state.friends.map((elem, index) => {
       return (
-         <li className="list-group-item" key={index}>
+         <li className="list-group-item" key={index} onClick={()=>this.handleInfo(index)}>
            {elem}
            <span className="badge">Connected</span>
          </li>
@@ -67,7 +88,7 @@ class ConnectwithFriends extends Component {
   }
 
   displayAllUsers = () => {
-    console.log(this.state.allUsers)
+    //console.log(this.state.allUsers)
     return this.state.allUsers.map((elem, index) => {
       const bool = this.state.friends.includes(elem);
       return (
@@ -80,7 +101,28 @@ class ConnectwithFriends extends Component {
     });
   }
 
+  friendInfo = (friend) => {
+    return this.UserInfoService.getUser(friend)
+    .then(user => {
+      console.log('friend Info:');
+      console.log(user);
+      const apiList = user.apiLogin;
+      const apiObj = apiList.find(elem => {
+        return elem.name === 'piazza';
+      })
+      console.log('friend api object: ')
+      console.log(apiObj);
+      //console.log(typeof apiObj === 'object');
+      if(typeof apiObj !== 'object') {
+        return (<h3>No Similar Classes</h3>)
+      }
+    })
+
+  }
+
   render() {
+    console.log(this.state);
+    const { friends, allUsers, friendModal } = this.state;
     return (
       <div className='col-md-4'>
 
@@ -101,6 +143,12 @@ class ConnectwithFriends extends Component {
             </ul>
           </div>
         </div>
+
+        <Modal open={friendModal[0]} onClose={this.handleCloseModal} little>
+          <h1>{friendModal[1]}</h1>
+          <hr />
+          {friendModal[0] && this.friendInfo(friendModal[1])}
+        </Modal>
       </div>
 
     )
